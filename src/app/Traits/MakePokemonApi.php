45b76682e33bb2126c54b8b66d92c0ace2cd1810
takuya178 <takuya.u1005@gmail.com>
2022-11-23 21:39:01 +0900

    add getName

--- src/app/Traits/MakePokemonApi.php
+++ src/app/Traits/MakePokemonApi.php
@@ -11,7 +11,16 @@ trait MakePokemonApi
     $client = new Client();
     $response = $client->request($method, $requestUrl);
     $response = json_decode($response->getBody()->getContents());
-    
-    return $response;
+    return $this->getName($response);
+  }
+
+  public function getName($response)
+  {
+    $json = file_get_contents($response->species->url);
+    $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+    return $json;
+    // return array_filter($response, function($res) {
+    //   return $res->species->names->language->name = "ja-Hrkt";
+    // });
   }
 }
